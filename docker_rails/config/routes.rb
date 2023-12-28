e420c3b3960091d7a534824b75cce4e291a63230
TakumiSenaha <oretatchun.no.10@gmail.com>
2023-12-28 14:53:15 +0900

    add route <get 'search/data', to: 'search#search_data'> to routes.rb

--- docker_rails/config/routes.rb
+++ docker_rails/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :classes, only: [:index]
   get 'search', to: 'search#index'
+  get 'search/data', to: 'search#search_data'
+
 end
