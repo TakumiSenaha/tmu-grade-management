538ba3279bdd235686da1eb6189873c9417ad538
TakumiSenaha <oretatchun.no.10@gmail.com>
2023-12-28 14:51:57 +0900

    add def search_data to seach_controller.rb

--- docker_rails/app/controllers/search_controller.rb
+++ docker_rails/app/controllers/search_controller.rb
@@ -2,9 +2,11 @@
 class SearchController < ApplicationController
   def index
     # パラメータに応じて検索を行う
+    # 検索結果をインスタンス変数に格納する（ビューで使用するため）
     @lectures = Lecture.search(search_params)
 
-    render json: @lectures
+    # 検索結果をJSON形式で返す(すべてのデータが返ってきてしまう)
+    # render json: @lectures
   end
 
   private
@@ -13,4 +15,10 @@ class SearchController < ApplicationController
     params.permit(:year, :term, :day, :period, :teacher, :name, :lecture_number, :credits, :subject_type, :faculty_code)
     # 必要に応じて他のパラメータも許可
   end
+
+  def search_data
+    @lectures = Lecture.search(params)
+    render json: @lectures
+  end
+
 end
