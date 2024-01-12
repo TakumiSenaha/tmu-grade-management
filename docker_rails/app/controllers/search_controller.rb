0ffcab02ccbfbb2242d5ff57ff21231d1d6b1f05
TakumiSenaha <oretatchun.no.10@gmail.com>
2024-01-12 19:25:21 +0900

    fix search_controller.rb

--- docker_rails/app/controllers/search_controller.rb
+++ docker_rails/app/controllers/search_controller.rb
@@ -3,23 +3,21 @@ class SearchController < ApplicationController
   def index
     # パラメータに応じて検索を行う
     # 検索結果をインスタンス変数に格納する（ビューで使用するため）
-    @lectures = Lecture.search(search_params)
-
+    # @lectures = Lecture.search(search_params)
     # 検索結果をJSON形式で返す(すべてのデータが返ってきてしまう)
     # render json: @lectures
   end
-
+  # def search_data
+  #   @lectures = Lecture.search(params)
+  #   render json: @lectures
+  # end
   def search_data
-    @lectures = Lecture.search(params)
+    @lectures = Lecture.search(search_params)
     render json: @lectures
   end
-
   private
-
   def search_params
     params.permit(:year, :term, :day, :period, :teacher, :name, :lecture_number, :credits, :subject_type, :faculty_code)
     # 必要に応じて他のパラメータも許可
   end
-
-
 end
